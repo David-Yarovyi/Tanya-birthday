dbb63a02d9e94acc40036e72c87f383d5e4d4077
Yaroviy David <david@MacBook-Air-Yaroviy.local>
2025-12-30 01:00:07 +0200

    Update files

--- handlers/videos_menu.py
+++ handlers/videos_menu.py
@@ -1,6 +1,6 @@
 from aiogram import F, Router, types
 from keyboards.main_menu import main_menu_kb
-from data.photos_date.photos import PHOTOS
+from data.videos_date.videos import VIDEOS
 from keyboards.photo_and_videos import show_photos_kb, back_to_photo_menu
 from aiogram.types import FSInputFile
 
@@ -8,7 +8,18 @@ router = Router()
 
 @router.callback_query(F.data == 'video')
 async def show_video_menu(callback : types.CallbackQuery):
-    await callback.message.edit_text(
-        text='Видео-поздравление 🎬',
+    await callback.message.delete()
+
+    await callback.message.bot.send_video(
+        chat_id=callback.message.chat.id,
+        video=FSInputFile(VIDEOS[0]),
+        caption='Видео-поздравление 🎬' 
+    )
+
+    await callback.message.bot.send_message(
+        chat_id=callback.message.chat.id,
+        text='⬅ Вернуться назад',
         reply_markup=back_to_photo_menu()
     )
+
+    await callback.answer()
